95debaf5faa7f607fc7ddec447d30d9d0b9c9691
Ahmad <abilalh7@gmail.com>
2026-01-12 22:49:47 -0600

    Header was restructured such that it gets its arguments from HeaderLayout without directly accessing useAuth; also AuthButtons.module.css was created

--- frontend/src/layouts/HeaderLayout.jsx
+++ frontend/src/layouts/HeaderLayout.jsx
@@ -1,12 +1,36 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header.jsx";
+import useAuth from "../contexts/AuthContext.jsx";
+import { useState } from "react";
 
 
-// HeaderLayout component to include Header on all pages
+
+// HeaderLayout component to include Header in all pages inside this layout
 export default function HeaderLayout( children, props ) {
+
+    const { isLoggedIn, handleLogout } = useAuth();
+
+    // new state that are transfered from HomePage.jsx
+    const[showRegistrationForm, setShowRegistrationForm] = useState(false);
+    const[showLoginForm, setShowLoginForm] = useState(false);
+
+
+
     return (
         <>
-            <Header />
+            <Header handleRegistration={ () => {
+                        setShowRegistrationForm(true);
+                        setShowLoginForm(false); 
+                    }}
+
+                    handleLogin={ () => {
+                        setShowLoginForm(true);
+                        setShowRegistrationForm(false);
+                    }}   
+                    
+                    handleLogout={handleLogout}
+                    isLoggedIn={isLoggedIn}
+             />
             <Outlet />
         </>
     );
