d84070b8258d455dfc33de3548ffda37e4319c5b
Ahmad <abilalh7@gmail.com>
2026-01-27 22:01:36 -0600

    Completed a working form and created a handleSubmit function in EmailInput.jsx frontend. It needs to be connected to backend endpoints and logic

--- frontend/src/components/Checkout/EmailInput.jsx
+++ frontend/src/components/Checkout/EmailInput.jsx
@@ -1,10 +1,44 @@
+import { API_BASE_URL } from '../../config';
 import styles from './EmailInput.module.css'
+import axios from 'axios';
 
 
 
-export default function EmailInput() {
+// This method gets the email of the user and send it to database whether it exists or not. 
+export default function EmailInput( {setUserHasAccount} ) {
+
+    async function handleSubmit(e) {
+              
+        e.preventDefault();
+
+        // Read form data
+        const form = e.target;
+        const formData = new FormData(form);
+
+        try {
+          
+          // TODO: fix this url!
+          const response = await axios.post(`${API_BASE_URL}/api`, formData);
+          if (response === 200) {
+            setUserHasAccount(true)
+          } else {
+            setUserHasAccount(false);
+          }
+
+        } catch (error) {
+          console.log("The email is not correct, please try again")
+        }
+
+    } 
+
+
+
+
+
 
     return (
+
+
       <div className={styles.mainContainerLayout}>
 
         <div className={styles.mainContainer} >
@@ -13,8 +47,9 @@ export default function EmailInput() {
             <h3>Sign in or create an account</h3>
             <h4>Enter your email address </h4>
 
-            <form>
+            <form onSubmit={handleSubmit}>
               <input name="userEmail" />
+              <button className={styles.submitButton} type='submit'>Continue</button>
             </form>
 
 
