6ce3aa6e4a4e97d1569269cfe8b884e9d7ceed45
Ahmad <abilalh7@gmail.com>
2026-02-19 22:06:57 -0600

    modified Address model in backend

--- backend/bookStoreApp/models.py
+++ backend/bookStoreApp/models.py
@@ -31,6 +31,8 @@ class Address(models.Model):
     zip_code = models.CharField(max_length=20)
     country = models.CharField(max_length=100)
 
+    last_used = models.DateTimeField(auto_now=True, null=True, blank=True)
+
     def __str__(self):
         return f"{self.street}, {self.city}, {self.state}, {self.zip_code}, {self.country}"
     
