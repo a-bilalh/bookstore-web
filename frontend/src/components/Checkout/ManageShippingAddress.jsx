a45c80a92a237b4c1aceda58410ce9959e5fb24e
Ahmad <abilalh7@gmail.com>
2026-02-26 22:50:19 -0600

    Created InputShippingAddress.jsx in Checkout components

--- frontend/src/components/Checkout/ManageShippingAddress.jsx
+++ frontend/src/components/Checkout/ManageShippingAddress.jsx
@@ -1,11 +1,20 @@
 import {useState, useEffect } from 'react';
 import fetchUserAddresses from '../../services/fetchUserAddresses';
+import InputShippingAddress from './InputShippingAddress';
+
 
 
 
 export default function ManageShippingAddress() {
 
+    // state to store user addresses
     const [addresses, setAddresses] = useState([]);
+
+    // state to trace if user is adding a new address 
+    const [showAddressForm, setShowAddressForm] = useState(false);
+
+    // state to store number of addresses displayed to the user, default is 5
+    const [numberOfAddressesDispalyed, setNumberOfAddressesDisplayed] = useState(5);
 
     // number of addresses the user has
     const numberOfAddresses = addresses.length;
@@ -31,9 +40,15 @@ export default function ManageShippingAddress() {
 
             <h5>Shipping Address</h5>
 
+            { numberOfAddresses === 0 && <button onClick={() => setShowAddressForm(true)}>add new address</button>}
             { numberOfAddresses > 0 && <button>change</button> }
 
-            <h5>show more</h5>
+
+            { numberOfAddresses > numberOfAddressesDispalyed && <button>show more</button>}
+
+            { showAddressForm && <InputShippingAddress action="add" />}
+
+            {}
 
         </div>
 
