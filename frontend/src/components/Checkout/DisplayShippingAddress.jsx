82b33ec4f20f1ad2b962848b2d63e1f697eb0461
Ahmad <abilalh7@gmail.com>
2026-03-01 13:41:59 -0600

    Created ManageShippingAddress.module.css

--- frontend/src/components/Checkout/DisplayShippingAddress.jsx
+++ frontend/src/components/Checkout/DisplayShippingAddress.jsx
@@ -6,7 +6,7 @@ export default function DisplayShippingAddress( { address } ) {
     return (
 
         <div>
-            <p>{address.street_address}</p>
+            <p>{address.street}</p>
             <p>{address.city}, {address.state} {address.zip_code}</p>
         </div>  )
 
