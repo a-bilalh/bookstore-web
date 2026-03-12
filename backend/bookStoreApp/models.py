bb7fe33db3af73d90fa77cc6731183fc92e47ad0
Ahmad <abilalh7@gmail.com>
2026-03-11 23:40:53 -0500

    Created Payment model in backend and created stripe_webhook function in views.py

--- backend/bookStoreApp/models.py
+++ backend/bookStoreApp/models.py
@@ -114,3 +114,14 @@ class Cart(models.Model):
         return f"Cart of {self.user.username}"
     
 
+
+# Payment model
+class Payment(models.Model):
+
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    stripe_payment_intent_id = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    payment_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Payment for Order {self.order.id} - Amount: {self.amount}"
