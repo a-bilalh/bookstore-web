4e204cf986c99dd78c22758ec117a2d3a3998161
Ahmad <abilalh7@gmail.com>
2026-03-19 17:26:47 -0500

    Search box is working perfectly now. The search result are clearly seperated and when clicked on, it takes user to bookDetails page

--- frontend/src/components/Header/SearchBar.jsx
+++ frontend/src/components/Header/SearchBar.jsx
@@ -3,14 +3,22 @@ import { useEffect, useState } from 'react';
 import styles from './SearchBar.module.css';
 import axios from 'axios';
 import { API_BASE_URL } from '../../config';
+import { useNavigate } from 'react-router-dom';
 
 
 // Search Bar Component
 export default function SearchBar() {
 
-
+    const navigate = useNavigate();
     const [query, setQuery] = useState("");
     const [results, setResults] = useState([]);
+
+
+    function openBookDetailsPage(book) {
+        navigate(`/book/${book.id}`)
+    }
+
+
 
     useEffect(() => {
 
@@ -46,7 +54,7 @@ export default function SearchBar() {
             <ul className={styles.searchResult}>
                 {
                     results.map((book) => (
-                        <li key={book.id}>
+                        <li key={book.id} onClick={() => openBookDetailsPage(book)}>
                             {book.title} - {book.author}
                         </li>
                     ))
